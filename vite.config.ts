f6f147c1b96b2064d9c7d713154721705e903b3a
forminiice <me@iice.fun>
2023-06-01 23:29:40 +0800

    feat: add markdown anchor

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,31 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Markdown from 'vite-plugin-md'
+import MarkdownAnchor from 'markdown-it-anchor'
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => {
   const glsl = (await import('vite-plugin-glsl')).default
 
   return {
-    plugins: [vue({ include: [/\.vue$/, /\.md$/] }), glsl(), Markdown()],
+    plugins: [
+      vue({ include: [/\.vue$/, /\.md$/] }),
+      glsl(),
+      Markdown({
+        markdownItOptions: {
+          html: true,
+          linkify: true,
+          typographer: true
+        },
+        markdownItSetup(md) {
+          md.use(MarkdownAnchor, {
+            permalink: true,
+            permalinkBefore: true,
+            permalinkSymbol: '#'
+          })
+        }
+      })
+    ],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
