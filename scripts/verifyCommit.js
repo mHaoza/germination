9fd023f46e171cb952ed926d6f8d99ab647cb29d
forminiice <me@iice.fun>
2023-06-03 12:31:05 +0800

    build(git hooks): change husky to simple-git-hooks

---
+++ scripts/verifyCommit.js
@@ -0,0 +1,29 @@
+// @ts-check
+import chalk from 'chalk'
+import { readFileSync } from 'fs'
+import path from 'path'
+
+chalk.level = 1
+
+const msgPath = path.resolve('.git/COMMIT_EDITMSG')
+const msg = readFileSync(msgPath, 'utf-8').trim()
+
+const commitRE =
+  /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
+
+if (!commitRE.test(msg)) {
+  console.error(
+    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(
+      `invalid commit message format.`
+    )}\n\n` +
+      chalk.red(
+        `  Proper commit message format is required for automated changelog generation. Examples:\n\n`
+      ) +
+      `    ${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
+      `    ${chalk.green(
+        `fix(v-model): handle events on blur (close #28)`
+      )}\n\n` +
+      chalk.red(`  See .github/commit-convention.md for more details.\n`)
+  )
+  process.exit(1)
+}
